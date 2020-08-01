3b2d1825ab4271562c69dd984cd902423fe7d2f5
Nurulla <numuminov@gmail.com>
2020-08-01 17:26:30 -0400

    added First Git Practice

---
+++ src/git/GitPractice.java
@@ -0,0 +1,13 @@
+package git;
+
+public class GitPractice {
+
+    public static void main(String[] args) {
+        String str1 = "ffr";
+        String str2 = "ggt";
+        String str3 = str1 + str2;
+
+        System.out.print(str3);
+
+    }
+}
